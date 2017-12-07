a8eb3e4ada1de5501696da0583e8b5a815c481fe
Andres Yepes <yepes07@gmail.com>
2017-12-06 23:03:40 -0500

    day 1 solution 2

--- lib/day1.ex
+++ lib/day1.ex
@@ -1,5 +1,5 @@
 defmodule Day1 do
-  def solution(input) when is_binary(input) do
+  def solution1(input) when is_binary(input) do
     input_list = String.graphemes(input)
     input_list_numbers = Enum.map input_list, fn (num)->
       {num, _} = Integer.parse(num)
@@ -7,13 +7,13 @@ defmodule Day1 do
     end
 
     sum = circular_validation(input_list_numbers)
-    day1_solution(input_list_numbers, sum)
+    day1_solution1(input_list_numbers, sum)
   end
 
-  defp day1_solution([first, first | []], sum), do: first + sum
-  defp day1_solution([_first, _second | []], sum), do: sum
-  defp day1_solution([first, first | rest] = input, sum) when is_list(input), do: day1_solution([first | rest], sum + first)
-  defp day1_solution([_first, second | rest] = input, sum) when is_list(input), do: day1_solution([second | rest], sum)
+  defp day1_solution1([first, first | []], sum), do: first + sum
+  defp day1_solution1([_first, _second | []], sum), do: sum
+  defp day1_solution1([first, first | rest] = input, sum) when is_list(input), do: day1_solution1([first | rest], sum + first)
+  defp day1_solution1([_first, second | rest] = input, sum) when is_list(input), do: day1_solution1([second | rest], sum)
 
   defp circular_validation(input) when is_list(input) do
     first = List.first(input)
@@ -23,5 +23,32 @@ defmodule Day1 do
     else
       0
     end
+  end
+
+  def solution2(input) when is_binary(input) do
+    input_list = String.graphemes(input)
+    input_list_numbers = Enum.map input_list, fn (num)->
+      {num, _} = Integer.parse(num)
+      num
+    end
+
+    day1_solution2(input_list_numbers)
+  end
+
+  defp day1_solution2(input) when is_list(input) do
+    length = length(input)
+    last_index = length - 1
+    Enum.reduce(0..last_index, 0, fn i, acc ->
+      first = Enum.at(input, i)
+      second_index = round(i + (length/2))
+      if last_index < second_index, do: second_index = second_index - last_index - 1
+      second = Enum.at(input, second_index)
+
+      if first == second do
+        acc + first
+      else
+        acc
+      end
+    end)
   end
 end
--- test/aoc_test.exs
+++ test/aoc_test.exs
@@ -2,23 +2,48 @@ defmodule AocTest do
   use ExUnit.Case
   doctest Aoc
 
-  test "Day1.solution(1122)" do
-    assert Aoc.day1_solution("1122") == 3
+  test "Day1.solution1(1122)" do
+    assert Aoc.day1_solution1("1122") == 3
   end
 
-  test "Day1.solution(1111)" do
-    assert Aoc.day1_solution("1111") == 4
+  test "Day1.solution1(1111)" do
+    assert Aoc.day1_solution1("1111") == 4
   end
 
-  test "Day1.solution(1234)" do
-    assert Aoc.day1_solution("1234") == 0
+  test "Day1.solution1(1234)" do
+    assert Aoc.day1_solution1("1234") == 0
   end
 
-  test "Day1.solution(91212129)" do
-    assert Aoc.day1_solution("91212129") == 9
+  test "Day1.solution1(91212129)" do
+    assert Aoc.day1_solution1("91212129") == 9
   end
 
-  test "Day1.solution(Aoc.day1_input())" do
-    assert Aoc.day1_solution(Aoc.day1_input) == 1119
+  test "Day1.solution1(Aoc.day1_input())" do
+    assert Aoc.day1_solution1(Aoc.day1_input) == 1119
+  end
+
+  test "Day1.solution2(1212)" do
+    assert Aoc.day1_solution2("1212") == 6
+  end
+
+  test "Day1.solution2(1221)" do
+    assert Aoc.day1_solution2("1221") == 0
+  end
+
+  test "Day1.solution2(123425)" do
+    assert Aoc.day1_solution2("123425") == 4
+  end
+
+  test "Day1.solution2(123123)" do
+    assert Aoc.day1_solution2("123123") == 12
+  end
+
+  test "Day1.solution2(12131415)" do
+    assert Aoc.day1_solution2("12131415") == 4
+  end
+
+  test "Day1.solution2(Aoc.day1_input())" do
+    IO.inspect Aoc.day1_solution2(Aoc.day1_input)
+    # assert Aoc.day1_solution1(Aoc.day1_input) == 1119
   end
 end
